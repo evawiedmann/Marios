98f14e4f4551bd7cc13e60beda3d303c305a267c
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-17 12:28:22 -0800

    add review and product class CRUD

--- app/controllers/reivews_controller.rb
+++ app/controllers/reivews_controller.rb
@@ -1,50 +1,57 @@
 class ReviewsController < ApplicationController
-
-  def index
-    @review = Review.all
-    render :index
-  end
+  #
+  # def index
+  #   @review = Review.all
+  #   render :index
+  # end
 
   def new
+    @product = Product.find(params[:product_id])
     @review = Review.new
-    render :new  end
+    render :new
+  end
 
-    def create
-      @review = Review.new(review_params)
-      if @review.save
-        redirect_to reviews_path
-      else
-        render :new
-      end
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
     end
+  end
 
-    def edit
-      @review = Review.find(params[:id])
+  # Other controller actions go here.
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to product_path(@review.product)
+    else
       render :edit
     end
-
-    def show
-      @review = Review.find(params[:id])
-      render :show
-    end
-
-    def update
-      @review= Review.find(params[:id])
-      if @review.update(review_params)
-        redirect_to reviews_path
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @review = Review.find(params[:id])
-      @review.destroy
-      redirect_to reviews_path
-    end
-    private
-    def review_params
-      params.require(:review).permit(:name, :genre)
-    end
-
   end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:name, :genre)
+  end
+end
