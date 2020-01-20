b268914e2c922ced997e3e5e7297c1b7a2455387
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-20 14:52:16 -0800

    fix spelling error

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_191553) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "cost"
+    t.float "cost"
     t.string "country_of_origin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
