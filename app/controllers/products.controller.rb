e24cc96720a62c993ea55b82beb1a22afba2d400
Eva Wiedmann <evawiedmann@gmail.com>
2020-01-20 14:23:33 -0800

    add flash message

--- app/controllers/products.controller.rb
+++ app/controllers/products.controller.rb
@@ -31,6 +31,7 @@ class ProductsController < ApplicationController
     def update
       @product= Product.find(params[:id])
       if @product.update(product_params)
+        flash[:notice] = "Product updated!"
         redirect_to products_path
       else
         render :edit
